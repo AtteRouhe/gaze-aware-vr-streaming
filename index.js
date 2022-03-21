1d90ce33c9e2ea10320a770f5e55488c10819cc8
ashlamp08 <dhungana.ashok08@gmail.com>
2022-03-21 15:34:01 +0200

    Added CLient

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const cors = require('cors');
+const cors = require("cors");
 
 // enable cors, serve files from /video
-app.use(cors())
-app.use(express.static('video'))
+app.use(cors());
+app.use("/video", express.static(__dirname + "/video"));
 
 app.listen(8000, function () {
-    console.log("Listening on port 8000!");
+  console.log("Listening on port 8000!");
 });
